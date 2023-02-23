c39c507a5a598eb71dd9becbd98bcd04758eee65
Stef Dunlap <stef@kindrobot.ca>
2023-02-22 20:57:38 -0500

    Initial commit

---
+++ init.lua
@@ -0,0 +1,26 @@
+local wibox = require("wibox")
+local awful = require("awful")
+local naughty = require("naughty")
+
+local function create_weather_widget(location)
+    local weather_widget = wibox.widget {
+        widget = wibox.widget.textbox,
+    }
+
+    awful.widget.watch("curl -s 'wttr.in/" .. location .. "?format=%c%t'", 300, function(widget, stdout)
+        widget:set_text(stdout)
+    end, weather_widget)
+
+    weather_widget:connect_signal("button::press", function(_, _, _, button)
+        if button == 1 then
+            awful.util.spawn("xfce4-terminal -T weather -He 'curl https://wttr.in/'" .. location, {
+                floating = true,
+                maximized = true,
+            })
+        end
+    end)
+
+    return weather_widget
+end
+
+return create_weather_widget
